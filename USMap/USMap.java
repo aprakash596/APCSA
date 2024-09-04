b1d4df93d5ae201c925de1438fe93e29a839203c
Aarav Prakash <aaravprakash3.14@gmail.com>
2024-09-04 10:06:20 -0700

    Uploading USMap unfinished programs

---
+++ USMap/USMap.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+/*
+ *	Creates a map of the United States with highly populated cities
+ * 	having a larger presence in the map
+ * 
+ *	@author Aarav Prakash
+ *	@since September 4, 2024
+ */
+ 
+public class USMap
+{
+	/**
+	  *		Main method used to get out of a static method
+	  */
+	public static void main (String[]args)
+	{
+		USMap usm = new USMap();
+		usm.runner();
+	}
+	public void runner()
+	{
+		FileUtils inFile = new FileUtils();
+		Scanner cities = inFile.openToRead("cities.txt");
+		Scanner bigCities = inFile.openToRead("bigCities.txt");
+		setUpCanvas();
+		drawCities(cities);
+	}
+	public void drawCities(Scanner city)
+	{
+		StdDraw.setPenRadius(0.05);
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.point(0.5, 0.5);
+	}
+	public void setUpCanvas()
+	{
+		StdDraw.setTitle("USMap");
+		StdDraw.setCanvasSize(900, 512);
+		StdDraw.setXscale(128.0, 65.0);
+		StdDraw.setYscale(22.0, 52.0);
+	}
+}
