068b9b94a8303d428afc419f9da40e6f5df9b4c3
Aarav Prakash <aaravprakash3.14@gmail.com>
2024-11-15 00:11:06 -0800

    Uploading HTMLUtilitiesP1
    includes examples for part 2

--- HTMLUtilitiesP2/HTMLUtilities.java
+++ HTMLUtilitiesP2/HTMLUtilities.java
@@ -1,1 +1,167 @@
+/**
+ *	Utilities for handling HTML
+ *
+ *	@author	Aarav Prakash
+ *	@since	November 1, 2024
+ */
+public class HTMLUtilities 
+{
+	
+	//	an array containing all of the puncutation
+	private final char[] PUNCTUATION = new char[]{'.', ',', ';', ':', '(', 
+		')', '?', '!', '=', '&', '~', '+','-'};
 
+	/**
+	 *	Break the HTML string into tokens. The array returned is
+	 *	exactly the size of the number of tokens in the HTML string.
+	 *	Example:	HTML string = "Goodnight moon goodnight stars"
+	 *				returns { "Goodnight", "moon", "goodnight", "stars" }
+	 *	@param str			the HTML string
+	 *	@return				the String array of tokens
+	 */
+	public String[] tokenizeHTMLString(String str) {
+		// make the size of the array large to start
+		String[] result = new String[10000];
+		for(int i = 0; i < result.length; i++)
+			result[i] = "";
+
+		int resultIndex = 0;
+		
+		while(str.length() > 0)
+		{
+			resultIndex = 0;
+			String token = "";
+			str = str.trim();
+
+			
+			if(str.charAt(0) == '<')
+				token = tokenizeTag(str);
+			else if((str.charAt(0) >= 'a' && str.charAt(0) <= 'z') ||
+				(str.charAt(0) >= 'A' && str.charAt(0) <= 'Z'))
+				token = tokenizeString(str);
+			else if(isPunctuation(str))
+				token = "" + str.charAt(0);
+			else
+				token = tokenizeNumber(str);
+
+			while(resultIndex < result.length && result[resultIndex].length() > 0)
+				resultIndex++;
+
+			result[resultIndex] = token;
+
+			str = str.substring(token.length());
+		}
+		
+		String[]placeHolder = result;
+		result = new String[resultIndex+1];
+
+		for(int i = 0; i < result.length; i++)
+			result[i] = placeHolder[i];
+
+		return result;
+	}
+
+	/**
+	 * This tokenizes the first number within the string
+	 * 
+	 * @param str	the HTML string
+	 * @return	the first number in the string
+	 */
+	public String tokenizeNumber(String str)
+	{
+		String number = "";
+		int stringIndex = 0;
+
+		if(str.charAt(stringIndex) == '-') 
+		{
+			number += str.charAt(stringIndex);
+			stringIndex++;
+		}
+
+		while(stringIndex < str.length())
+		{
+			char stringChar = str.charAt(stringIndex);
+
+			if((stringChar >= '0' && stringChar <= '9') || stringChar == '.' || 
+				stringChar == 'e')
+				number += "" + stringChar;
+			else
+				stringIndex = str.length();
+
+			stringIndex++;
+		}
+		return number;
+	}
+	
+	/**
+	 * Checks if a string starts with punctuation
+	 * 
+	 * @param str	the HTML string
+	 * @return	if the token at the beginning is punctuation
+	 */
+	public boolean isPunctuation(String str)
+	{
+		boolean punctuation = false;
+		for(int i = 0; i < PUNCTUATION.length; i++)
+		{
+			if(str.charAt(0) == PUNCTUATION[i])
+			{
+				if(PUNCTUATION[i] == '-' && str.length() != 1
+					&& (str.charAt(1) >= '0' && str.charAt(1) <= '9'))
+					punctuation = false;
+				else
+					punctuation = true;
+			}
+		}
+		return punctuation;
+	}
+	
+	/**
+	 * Tokenizes the first tag in the inputted string
+	 * 
+	 * @param str	the HTML string
+	 * @return	the first tag in the string
+	 */
+	public String tokenizeTag(String str)
+	{
+		return str.substring(str.indexOf('<'),str.indexOf('>')+1);
+	}
+
+	/**
+	 * Tokenizes a word from the HTML string
+	 * 
+	 * @param str	the HTML string
+	 * @return	the first tokenized string
+	 */
+	public String tokenizeString(String str)
+	{
+		String word = "";
+		String lowerStr = str.toLowerCase();
+		for(int i = 0; i < str.length(); i++)
+		{
+			if((lowerStr.charAt(i) >= 'a' && lowerStr.charAt(i) <= 'z')
+				|| lowerStr.charAt(i) == '-')
+				word += "" + str.charAt(i);
+			else
+				i = str.length();
+		}
+		
+		return word;
+	}
+	
+	/**
+	 *	Print the tokens in the array to the screen
+	 *	Precondition: All elements in the array are valid String objects.
+	 *				(no nulls)
+	 *	@param tokens		an array of String tokens
+	 */
+	public void printTokens(String[] tokens) {
+		if (tokens == null) return;
+		for (int a = 0; a < tokens.length; a++) {
+			if (a % 5 == 0) System.out.print("\n  ");
+			System.out.print("[token " + a + "]: " + tokens[a] + " ");
+		}
+		System.out.println();
+	}
+
+}
