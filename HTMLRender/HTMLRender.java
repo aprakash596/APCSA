224c7adac3e585bfcd6baa8d91d8ab6ba7de74bf
Aarav Prakash <aaravprakash3.14@gmail.com>
2024-11-20 10:04:50 -0800

    Uploading HTMLRender v1
    started on the states of tags, like bold or italic

--- HTMLRender/HTMLRender.java
+++ HTMLRender/HTMLRender.java
@@ -1,1 +1,174 @@
+/**
+ *	HTMLRender
+ *	This program renders HTML code into a JFrame window.
+ *	It requires your HTMLUtilities class and
+ *	the SimpleHtmlRenderer and HtmlPrinter classes.
+ *
+ *	The tags supported:
+ *		<html>, </html> - start/end of the HTML file
+ *		<body>, </body> - start/end of the HTML code
+ *		<p>, </p> - Start/end of a paragraph.
+ *					Causes a newline before and a blank line after. Lines are restricted
+ *					to 80 characters maximum.
+ *		<hr>	- Creates a horizontal rule on the following line.
+ *		<br>	- newline (break)
+ *		<b>, </b> - Start/end of bold font print
+ *		<i>, </i> - Start/end of italic font print
+ *		<q>, </q> - Start/end of quotations
+ *		<hX>, </hX> - Start/end of heading with size X = 1, 2, 3, 4, 5, 6
+ *		<pre>, </pre> - Preformatted text
+ *
+ *	@author
+ *	@version
+ */
+public class HTMLRender {
+	
+	// the array holding all the tokens of the HTML file
+	private String [] tokens;
+	private final int TOKENS_SIZE = 100000;	// size of array
+	
+	//	an array containing all of the puncutation
+	private final char[] PUNCTUATION = new char[]{'.', ',', ';', ':', '(', 
+		')', '?', '!', '=', '&', '~', '+','-'};
+		
+	private enum TagState {REGULAR, BOLD, ITALIC, NOPRINT};
+	
+	private TagState state = TagState.NOPRINT; 
 
+	// SimpleHtmlRenderer fields
+	private SimpleHtmlRenderer render;
+	private HtmlPrinter browser;
+	
+	private HTMLUtilities util;			// HTMLUtilities used in render
+		
+	public HTMLRender() {
+		// Initialize token array
+		tokens = new String[TOKENS_SIZE];
+		
+		// Initialize Simple Browser
+		render = new SimpleHtmlRenderer();
+		browser = render.getHtmlPrinter();
+		
+		util = new HTMLUtilities();
+	}
+	
+	
+	public static void main(String[] args) {
+		HTMLRender hf = new HTMLRender();
+		hf.run(args);
+	}
+	
+	public void run(String[]args) 
+	{
+		Scanner input = null;
+		String fileName = "";
+		if (args.length > 0)
+			fileName = args[0];
+		else {
+			System.out.println("Usage: java HTMLRender <htmlFileName>");
+			System.exit(0);
+		}
+		
+		readTokenFile(fileName);
+		removeEmpty();
+		printTokens();
+		
+		
+		/*
+		// Sample renderings from HtmlPrinter class
+		
+		// Print plain text without line feed at end
+		browser.print("First line");
+		
+		// Print line feed
+		browser.println();
+		
+		// Print bold words and plain space without line feed at end
+		browser.printBold("bold words");
+		browser.print(" ");
+		
+		// Print italic words without line feed at end
+		browser.printItalic("italic words");
+		
+		// Print horizontal rule across window (includes line feed before and after)
+		browser.printHorizontalRule();
+		
+		// Print words, then line feed (printBreak)
+		browser.print("A couple of words");
+		browser.printBreak();
+		browser.printBreak();
+		
+		// Print a double quote
+		browser.print("\"");
+		
+		// Print Headings 1 through 6 (Largest to smallest)
+		browser.printHeading1("Heading1");
+		browser.printHeading2("Heading2");
+		browser.printHeading3("Heading3");
+		browser.printHeading4("Heading4");
+		browser.printHeading5("Heading5");
+		browser.printHeading6("Heading6");
+		
+		// Print pre-formatted text (optional)
+		browser.printPreformattedText("Preformat Monospace\tfont");
+		browser.printBreak();
+		browser.print("The end");
+		*/
+	}
+	
+	public void readTokenFile(String fileName)
+	{
+		input = FileUtils.openToRead(fileName);
+		
+		int tokenCount = 0;
+		while (input.hasNext()) {
+			String line = input.nextLine();
+			String []tokenizedLine = util.tokenizeHTMLString(line);
+			for(int i = 0; i < tokenizedLine.length; i++)
+				tokens[tokenCount + i] = tokenizedLine[i];
+			
+			tokenCount += tokenizedLine.length;
+		}
+		
+		input.close();
+	}
+	
+	public void removeEmpty()
+	{
+		String[]placeHolder = tokens;
+
+		int count = 0;
+		while(placeHolder[count].length() > 0)
+			count++;
+
+		tokens = new String[count];
+
+		for(int i = 0; i < count; i++)
+			tokens[i] = placeHolder[i];
+	}
+	
+	public void printTokens()
+	{
+		for(int i = 0; i < tokens.length; i++)
+		{
+			String token = tokens[i];
+			if(token.indexOf('<') > -1 && token.indexOf('>') > -1)
+			{
+				token = token.toLowerCase();
+				if(token.equals("<html>") && token[i].equalsIgoreCase("<body>"))
+					state = TagState.REGULAR;
+				else if(token.equals("</body>") && token[i].equalsIgoreCase("</html>"))
+					state = TagState.NOPRINT;
+				else if(tagState != TagState.NOPRINT)
+				{
+					if(token.equals("<b>")
+						state = TagState.BOLD;
+					if(token.equals("<\b>") || token.equals("<\i>"))
+						state = TagState.REGULAR;
+					if(token.equals("<i>"))
+						state = TagState.ITALIC
+				}
+			}
+		}
+	}
+}
---
+++ HTMLRender/HTMLUtilities.java
@@ -0,0 +1,226 @@
+/**
+ *	Utilities for handling HTML. The goal of this program is to 
+ * 	separate the input HTML file into tokens, including HTML tags, 
+ * 	words, numbers, and punctuation. Furthermore, it excludes comments
+ * 	and accounts for formatting with specific tags.
+ *
+ *	@author	Aarav Prakash
+ *	@since	November 1, 2024
+ */
+public class HTMLUtilities 
+{
+	//	an array containing all of the puncutation
+	private final char[] PUNCTUATION = new char[]{'.', ',', ';', ':', '(', 
+		')', '?', '!', '=', '&', '~', '+','-'};
+
+	// NONE = not nested in a block, COMMENT = inside a comment block
+	// PREFORMAT = inside a pre-format block
+	private enum TokenState {NONE, COMMENT, PREFORMAT};
+
+	// the current tokenizer state
+	private TokenState state = TokenState.NONE; 
+
+	/**
+	 *	Break the HTML string into tokens. The array returned is
+	 *	exactly the size of the number of tokens in the HTML string.
+	 *	Example:	HTML string = "Goodnight moon goodnight stars"
+	 *				returns { "Goodnight", "moon", "goodnight", "stars" }
+	 *	@param str			the HTML string
+	 *	@return				the String array of tokens
+	 */
+	public String[] tokenizeHTMLString(String str) 
+	{
+		// make the size of the array large to start
+		String[] result = new String[10000];
+		for(int i = 0; i < result.length; i++)
+			result[i] = "";
+
+		int resultIndex = 0;
+		
+		while(str.length() > 0)
+		{
+			resultIndex = 0;
+			String token = "";
+
+			if(state != TokenState.PREFORMAT)
+				str = str.trim();
+
+			if(str.indexOf("<!--") == 0)
+				state = TokenState.COMMENT;
+
+			if(state == TokenState.COMMENT && str.indexOf("-->") > -1)
+			{
+				state = TokenState.NONE;
+				str = str.substring(str.indexOf("-->") + 3);
+			}
+			
+			if(state != TokenState.COMMENT && str.length() > 0)
+			{
+				if(str.charAt(0) == '<')
+				{
+					if(str.indexOf('>') > -1)
+						token = tokenizeTag(str);
+
+					if(token.equals("<pre>"))
+						state = TokenState.PREFORMAT;
+					if(token.equals("</pre>"))
+						state = TokenState.NONE;
+
+				}
+				else if((str.charAt(0) >= 'a' && str.charAt(0) <= 'z') ||
+					(str.charAt(0) >= 'A' && str.charAt(0) <= 'Z'))
+				{
+					token = tokenizeString(str);
+				}
+				else if(isPunctuation(str))
+					token = "" + str.charAt(0);
+				else
+					token = tokenizeNumber(str);
+			}
+
+			if(state == TokenState.PREFORMAT)
+			{
+				token = str;
+			}
+
+			while(resultIndex < result.length && result[resultIndex].length() > 0)
+				resultIndex++;
+
+			if(state != TokenState.COMMENT && token.length() != 0)
+			{
+				while(resultIndex < result.length && result[resultIndex].length() > 0)
+					resultIndex++;
+
+				result[resultIndex] = token;
+
+				str = str.substring(token.length());
+			}
+			else if(state == TokenState.COMMENT)
+				str = "";
+		}
+		
+		String[]placeHolder = result;
+
+		int count = 0;
+		while(placeHolder[count].length() > 0)
+			count++;
+
+		result = new String[count];
+
+		for(int i = 0; i < count; i++)
+			result[i] = placeHolder[i];
+
+		if(result.length > 0)
+			return result;
+		else
+			return null;
+	}
+
+	/**
+	 * This tokenizes the first number within a string
+	 * 
+	 * @param str	the HTML string
+	 * @return	the first number in the string
+	 */
+	public String tokenizeNumber(String str)
+	{
+		String number = "";
+		int stringIndex = 0;
+
+		if(str.charAt(stringIndex) == '-') 
+		{
+			number += str.charAt(stringIndex);
+			stringIndex++;
+		}
+
+		while(stringIndex < str.length())
+		{
+			char stringChar = str.charAt(stringIndex);
+
+			if((stringChar >= '0' && stringChar <= '9') || stringChar == '.' || 
+				stringChar == 'e')
+				number += "" + stringChar;
+			else
+				stringIndex = str.length();
+
+			stringIndex++;
+		}
+		return number;
+	}
+	
+	/**
+	 * Checks if a string starts with punctuation, excluding when the
+	 * punctuation is for a negative number
+	 * 
+	 * @param str	the HTML string
+	 * @return	if the token at the beginning is punctuation
+	 */
+	public boolean isPunctuation(String str)
+	{
+		boolean punctuation = false;
+		for(int i = 0; i < PUNCTUATION.length; i++)
+		{
+			if(str.charAt(0) == PUNCTUATION[i])
+			{
+				if(PUNCTUATION[i] == '-' && str.length() != 1
+					&& (str.charAt(1) >= '0' && str.charAt(1) <= '9'))
+					punctuation = false;
+				else
+					punctuation = true;
+			}
+		}
+		return punctuation;
+	}
+	
+	/**
+	 * Tokenizes the first tag in the inputted string
+	 * 
+	 * @param str	the HTML string
+	 * @return	the first tag in the string
+	 */
+	public String tokenizeTag(String str)
+	{
+		if(str.indexOf("<!--") != str.indexOf('<'))
+			return str.substring(str.indexOf('<'),str.indexOf('>')+1);
+		else
+			return "";
+	}
+
+	/**
+	 * Tokenizes a word from the HTML string
+	 * 
+	 * @param str	the HTML string
+	 * @return	a tokenized word
+	 */
+	public String tokenizeString(String str)
+	{
+		String word = "";
+		String lowerStr = str.toLowerCase();
+		for(int i = 0; i < str.length(); i++)
+		{
+			if((lowerStr.charAt(i) >= 'a' && lowerStr.charAt(i) <= 'z')
+				|| lowerStr.charAt(i) == '-')
+				word += "" + str.charAt(i);
+			else
+				i = str.length();
+		}
+		
+		return word;
+	}
+	
+	/**
+	 *	Print the tokens in the array to the screen
+	 *	Precondition: All elements in the array are valid String objects.
+	 *				(no nulls)
+	 *	@param tokens		an array of String tokens
+	 */
+	public void printTokens(String[] tokens) {
+		if (tokens == null) return;
+		for (int a = 0; a < tokens.length; a++) {
+			if (a % 5 == 0) System.out.print("\n  ");
+			System.out.print("[token " + a + "]: " + tokens[a] + " ");
+		}
+		System.out.println();
+	}
+
+}
